29ffcfc51ed71f4ea8964f602e41f3faafa92aee
bluptak <bluptak@uwaterloo.ca>
2010-11-01 21:24:00 -0400

    Must specify file extension

---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello World!" << endl;   
+  cout << "Welcome to C++ Programming" << endl;
+  cout << "This is a test to see if git push --all is necessary when pushing a file not yet existing on the repository" << endl;
+  return 0;
+  }
+
